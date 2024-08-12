4becd3848e1b72b9187fc01bc3a774091bc1365e
Eugenio <geustodj@gmail.com>
2024-08-12 16:52:05 -0500

    destructuring example

--- part1/1.6/src/App.jsx
+++ part1/1.6/src/App.jsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+
 const App = () => {
+  const [counter, setCounter] = useState(0)
 
-  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
+  const increaseByOne = () => {
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button onSmash={increaseByOne} text="plus" />
+      <Button onSmash={setToZero} text="zero" />
+      <Button onSmash={decreaseByOne} text="minus" />
+    </div>
   )
 }
 
