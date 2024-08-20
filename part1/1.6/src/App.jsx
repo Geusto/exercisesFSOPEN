b6fa5debc96c9372734189069769dbc3c9ecd22e
Eugenio <geustodj@gmail.com>
2024-08-20 15:47:00 -0500

    function that return function

--- part1/1.6/src/App.jsx
+++ part1/1.6/src/App.jsx
@@ -1,59 +1,21 @@
 import { useState } from 'react'
 
-// const Display = ({counter}) => <div>{counter}</div>
-
-// const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-
-const Button = (props) => {
-  console.log('props value is ', props)
-  const { handleClick, text } = props
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
   }
 
   return (
     <div>
-      {left}
+      {value}
 
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
