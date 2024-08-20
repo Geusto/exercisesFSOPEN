3489781c2d8195ccb193635be7348b0b12e4d0e7
Eugenio <geustodj@gmail.com>
2024-08-20 12:35:25 -0500

    array, async update example

--- part1/1.6/src/App.jsx
+++ part1/1.6/src/App.jsx
@@ -5,34 +5,35 @@ const Display = ({counter}) => <div>{counter}</div>
 const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  console.log('rendering with counter value', counter)
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => {
-
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
-  const decreaseByOne = () => { 
-
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onSmash={increaseByOne} text="plus" />
-      <Button onSmash={setToZero} text="zero" />
-      <Button onSmash={decreaseByOne} text="minus" />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }
