7db81b8a968be792ec1c4e674795d0d685ccf34d
Eugenio <geustodj@gmail.com>
2024-08-20 15:48:36 -0500

    Update App.jsx

--- part1/1.6/src/App.jsx
+++ part1/1.6/src/App.jsx
@@ -3,21 +3,24 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
 
-
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
     setValue(newValue)
   }
 
   return (
     <div>
       {value}
-
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }
-
 export default App
