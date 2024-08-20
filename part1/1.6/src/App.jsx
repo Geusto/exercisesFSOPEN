2f0076dbafa5febda002677a7907d7a3954e5cf8
Eugenio <geustodj@gmail.com>
2024-08-20 15:37:55 -0500

    conditional state

--- part1/1.6/src/App.jsx
+++ part1/1.6/src/App.jsx
@@ -1,39 +1,59 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+// const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+// const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = (props) => {
+  console.log('props value is ', props)
+  const { handleClick, text } = props
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
 
-  const [total, setTotal] = useState(0)
-
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(updatedRight + left)
+    setRight(right + 1)
   }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
 
-      <p>total {total}</p>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
