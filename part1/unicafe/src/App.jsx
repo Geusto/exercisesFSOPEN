1820adcb133bfa4477a1da202a206763380cf602
Eugenio <geustodj@gmail.com>
2024-08-20 21:15:29 -0500

    unicafe 1.7 exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  let total = bad+neutral+good
+  let positivePercent = (good/total)*100
+  if(total ==0){
+    positivePercent = 0
+  }
 
   return (
     <div>
@@ -21,7 +28,9 @@ const App = () => {
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>average {bad}</p>
+      <p>average {total/3}</p>
+      <p>positive {positivePercent}%</p>
     </div>
   )
 }
