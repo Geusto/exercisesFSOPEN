51003e7c22c65a4c509ab6b29b374439af72cec1
Eugenio <geustodj@gmail.com>
2024-08-26 16:38:18 -0500

    unicafe exercise 1.8 & 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,32 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) =>{
+
+  let total = bad+neutral+good
+  let positivePercent = (good/total)*100
+  if(total == 0){
+    positivePercent = 0
+  }
+  if(total == 0){
+    return <p>no feedback given</p>
+  }
+  return(
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>average {bad}</p>
+      <p>average {total/3}</p>
+      <p>positive {positivePercent}%</p>
+    </>
+  )
+}
+
 
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let total = bad+neutral+good
-  let positivePercent = (good/total)*100
-  if(total ==0){
-    positivePercent = 0
-  }
 
   return (
     <div>
@@ -26,11 +41,7 @@ const App = () => {
         bad
       </button>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>average {bad}</p>
-      <p>average {total/3}</p>
-      <p>positive {positivePercent}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
