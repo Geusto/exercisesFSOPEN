14ee92a7eb089a70875f044038bf3075681bc055
Eugenio <geustodj@gmail.com>
2024-09-02 22:40:07 -0500

    anecdotes exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
@@ -15,24 +15,20 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ];
-  
+
   const sizeAnecdotes = anecdotes.length;
   const [points, setPoints] = useState(Array(sizeAnecdotes).fill(0));
   const [selected, setSelected] = useState(0);
-  const [score, setScore] = useState(0);
-  
+  const [score, setScore] = useState({ text: '', votes: 0 })
+
   const handleVote = () => {
     const newPoints = [...points];
     newPoints[selected] += 1;
     setPoints(newPoints);
 
-    let mayor = newPoints[0]
-    for(let i of newPoints){
-      if(newPoints[i] > newPoints){
-        mayor = newPoints[i]
-        setScore(mayor)
-      }
-    }
+    const maxVotes = Math.max(...newPoints);
+    const maxIndex = newPoints.indexOf(maxVotes);
+    setScore({ text: anecdotes[maxIndex], votes: maxVotes });
   };
 
   const handleNextAnecdote = () => {
@@ -41,11 +37,20 @@ const App = () => {
 
   return (
     <>
+      <h2>Anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <Button text='vote' handleClick={handleVote} />
       <Button text='next anecdote' handleClick={handleNextAnecdote} />
-      <p>{score}</p>
+      <h2>Anecdote with most votes</h2>
+      {score.votes > 0 ? (
+        <>
+          <p>{score.text}</p>
+          <p>has {score.votes} votes</p>
+        </>
+      ) : (
+        <p>No votes yet</p>
+      )}
     </>
   );
 };
