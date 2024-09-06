b2c4227c10726ba1027d148f69a491c5e0363373
Eugenio <geustodj@gmail.com>
2024-09-05 22:32:22 -0500

    course exercise 2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,10 +2,12 @@ import Header from './Header'
 import Content from './Content'
 
 const Course = ({ course }) => {
+  const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
   return (
     <>
       <Header course={course} />
       <Content course={course} />
+      <p>total of {total} exercises</p>
     </>
   )
 }
